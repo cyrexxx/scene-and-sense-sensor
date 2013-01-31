1bac7325f97c1f1c9e24f300ef77c75234efa992
cyrexxx <cyrexxx@gmail.com>
2013-01-31 04:03:54 +0000

    Master - RF to Serial

---
+++ snappyImages/RF26x.py
@@ -0,0 +1,18 @@
+"""
+This program and the accompanying materials
+are made available under the terms of the Eclipse Public License v1.0
+which accompanies this distribution, and is available at
+http://www.eclipse.org/legal/epl-v10.html
+Contributor: Synapse Wireless Inc., Huntsville, Alabama, 35806, USA
+"""
+
+"""
+RF26x.py - IO definitions for the RF26x module family
+"""
+DIO12 = 21 # pin 4
+DIO10 = 20 # pin 6, RSSI PWM, 
+DIO11 = 19 # pin 7
+DIO7 = 12 # pin 12, CTS (output)
+DIO5 = 37 # pin 15, Associated Indicator
+DIO6 = 16 # pin 16, RTS (input)
+DIO0 = 28 # pin 20, Analog input 0, Commissioning
