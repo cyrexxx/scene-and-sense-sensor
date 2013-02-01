38955edcd576d0bea493b4a97211e9e8693dec1f
cyrexxx@gmail.com <cyrexxx@gmail.com@2b142abe-724d-d4c8-a59e-74b5107960c4>
2013-02-01 04:08:10 +0000

    Master untested

--- trunk/snappyImages/Flex_Master.py
+++ trunk/snappyImages/Flex_Master.py
@@ -28,6 +28,7 @@ numHops = 4
 secondCounter = 0
 @setHook(HOOK_STARTUP)
 def startup():
+
     # Initialize UART
     initUart(1, 9600)           # 9600 baud
     flowControl(1, False)       # No flow control
@@ -38,17 +39,22 @@ def startup():
     
     # Enable bridge connections on the other UART
     crossConnect(DS_UART0, DS_PACKET_SERIAL)
-
+    
+def findServer():
+    """Devices who WANT buzzer capability call this function"""
+    rpc(rpcSourceAddr(), 'serverAt', localAddr())
+    
 def announceMaster():
     """Broadcast master status.  This allows slaves to learn our address, so they can unicast back."""
-    mcastRpc(1, numHops, 'master')
+    #mcastRpc(1, numHops, 'master')
+    mcastRpc(1, numHops, 'serverAt',localAddr())
 
-@setHook(HOOK_100MS)    
-def poll100ms(mstick):
+@setHook(HOOK_1S)    
+def poll1ms(mstick):
     global secondCounter
     
     # Periodically announce 'master' status to all slaves
     secondCounter += 1
-    if secondCounter >= 10:
+    if secondCounter >= 5:
         announceMaster()
         secondCounter = 0
