66973ab6eb88f1b80b5606145c7af34fda155fec
cyrexxx <cyrexxx@gmail.com>
2013-02-02 04:55:54 +0000

    Slave Clean up

--- snappyImages/Flex_slave.py
+++ snappyImages/Flex_slave.py
@@ -7,55 +7,55 @@ Hardware: SM700 , Ver 2.4.33
 
 Description:
     this code reads Analog values for 8 flex sensors and encodes it 
-    into one message and sends it over the air to the Server  
+    into one message and sends it over the air to the Server 
+    Slave periodically searches for master, if none are found , it send
+    the data to Portal. 
 Include Files (Libs)
-     synapse.evalBase 
-     synapse.nvparams 
+     synapse.nvparams
+     synapse.platforms 
 *****************************************************************************/"""
 
 #Lib
-
-#from synapse.nvparams import *
+from synapse.platforms import *
+from synapse.nvparams import *
 #from string import *
 
-serverAddr = '\x00\x00\x01' # hard-coded address for Portal PC
+serverAddr = '\x00\x00\x01'  # hard-coded address for Portal (PC/MAC)
 
-#if platform != "SM700":
- #   compileError #script only valid on SM700
+if platform != "SM700":
+    compileError        #script only valid on SM700
 
-# Sensor connection PINs on board.
-
- 
 
 # Device address bits, 3 bits in total from DIP switch
 
-addrBit0 = 10
-addrBit1 = 9
-addrBit2 = 27
+addrBit0 = 10     # KB5
+addrBit1 = 9      #TMR1
+addrBit2 = 27     #TMR2
 
 def makeInput(pin):
     setPinDir(pin, False)   # set direction of the pin as output
-    #setPinPulldown(pin, True) # Power the pin for the Photocell
     monitorPin(pin, True)   # Monitor for button press
+  
     
 # Things to do at startup
 @setHook(HOOK_STARTUP)
 def startupEvent():
     global addreBits
-    findServer() ##in once server is ready 
     global acount 
     acount = 0
+    
+    findServer() ##in once server is ready 
+    
     # Set PIN directions and initialize
+    
     makeInput(addrBit0)
     makeInput(addrBit1)
     makeInput(addrBit2)
     setRate(1)             # set rate of polling for buttons 
+ 
+    addreBits = buttonRead() #initialize buttons, get ADD bits 
     
-
-    #convert address bits to intiger number 
-    addreBits = buttonRead()
-    #mcastRpc
-   
+       
 # Tries to find an active server
 def findServer():
     mcastRpc(1,5,'svrAddr')
@@ -64,61 +64,49 @@ def findServer():
 def serverAt(addr):
     global serverAddr,addset
     serverAddr = addr[:]
-    #serverAddr ="/x5E/x34/xAD"
     addset='set '+str(serverAddr)
-    #sendData(addset)
     mcastRpc(1,5,"logEvent",addset)
-    #rpc(serverAddr,'echo','test')
-    #ucastSerial(rpcSourceAddr())
     
-    
-def setThreshold(newThreshold):
-    #Use this to change the 'darkness' threshold from the default of 85%
-    global darkThreshold
-    darkThreshold = newThreshold
+#def setThreshold(newThreshold):
+    #Use this to change the  threshold from the default #Not used #future
+    #global Threshold
+    #Threshold = Threshold
 
-def setRange(newRange):
-    #Use this to change the default 'required light range' from the default of 100 ADC counts
-    global requiredRange
-    requiredrange = newRange
+#def setRange(newRange):
+    #Use this to change the default 'required light range' from the default #Not used #future
+    #global requiredRange
+    #requiredrange = newRange
     
 # Do every 10 MS    
 @setHook(HOOK_10MS)
 def timer10MSEvent(currentMs):
     global sens,inpstr
     
-    # Read in the Analog values from the Sensors
-    # read 8 sensor values
+    # Read in the 8 sensor analog values from the Sensors
+    # formating :sensor_number#ADC_value. .......
     sens =  ':1#' + str(ADC_0)  + '.2#' +  str(ADC_1) + '.3#' + str(ADC_2) + '.4#' +  str(ADC_3) + '.5#' +  str(ADC_4) + '.6#'+ str(ADC_5) + '.7#'+ str(ADC_6) + '.8#'+ str(ADC_7)+'.'
-    
-    """sadc = str(ADC_0)
-    sadc = str(ADC_1)
-    sadc = str(ADC_2)
-    sadc = str(ADC_3)
-    sadc = str(ADC_4)
-    sadc = str(ADC_5)
-    sadc = str(ADC_6)
-    sadc = str(ADC_7)"""
-    
+  
+    # formating  Slave_address + sens (:sensor_number#ADC_value. .......)
     inpstr= str(addreBits) + sens    # package the Values in to one msg
-    sendData(inpstr)
+    sendData(inpstr)                     #call function to broadcast data
     
+#fuct to broadcast received data to portal or master     
 def sendData(mdata):
-    if serverAddr == '\x00\x00\x01':
+    if serverAddr == '\x00\x00\x01':      # if no Marste is found send Msg to portal 
        rpc(serverAddr,"logEvent",mdata)
-    else:
+    else:                                 # if master found send data only to master
        rpc(serverAddr, "printData", mdata)
-       #mcastRpc(1,5,"logEvent",mdata)
-    
-    
+  
+# Do every 1 MS  
 @setHook(HOOK_1MS)
 def timer1MSEvent(currentMs):
     global ADC_0,ADC_1,ADC_2,ADC_3,ADC_4,ADC_5,ADC_6,ADC_7
     global acount 
     
+    # reading each ADC once every 9 MS
     # Using Vref=1.6 V
     if acount == 0:
-        ADC_0 = readAdc(9)   #ADC 0
+        ADC_0 = readAdc(9)    #ADC 0
     elif acount == 1:
         ADC_1 = readAdc(10)   #ADC 1
     elif acount == 2:
@@ -137,7 +125,7 @@ def timer1MSEvent(currentMs):
     
     acount+=1
        
-    
+# Do every time there is a change in any monitored PIN 
 @setHook(HOOK_GPIN)
 def buttonEvent(pinNum, isSet):    
      #Action taken when the on-board buttton is pressed (i.e. change address )
@@ -145,6 +133,8 @@ def buttonEvent(pinNum, isSet):
      if pinNum == (addrBit0 or addrBit1 or addrBit2):
         addreBits = buttonRead()
     
+    
+#convert address bits to intiger number 
 def buttonRead():
     return ((4*(readPin(addrBit0))) +(2*(readPin(addrBit1)))+(1*(readPin(addrBit2))))
     
